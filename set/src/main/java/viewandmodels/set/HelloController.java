1e5b1760be3edc7009ca3f6b8f00bcda519d8b93
Gabriela6666 <pietrasgabriela6@gmail.com>
2024-05-03 21:19:05 +0200

    test100

--- set/src/main/java/viewandmodels/set/HelloController.java
+++ set/src/main/java/viewandmodels/set/HelloController.java
@@ -3,12 +3,25 @@ package viewandmodels.set;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.util.Random;
+
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        String[] welcomeMessages = {
+                "Welcome to JavaFX Application!",
+                "Nice to see you here!",
+                "Hello, dear user!",
+                "Greetings from JavaFX!"
+        };
+
+        Random random = new Random();
+        int index = random.nextInt(welcomeMessages.length);
+        String randomWelcomeMessage = welcomeMessages[index];
+
+        welcomeText.setText(randomWelcomeMessage);
     }
 }

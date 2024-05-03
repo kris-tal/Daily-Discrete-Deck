1db307cfd231e08f6d07f7ac668c2af97525e467
Gabriela6666 <pietrasgabriela6@gmail.com>
2024-05-03 21:31:38 +0200

    test555

--- set/src/main/java/viewandmodels/set/Board.java
+++ set/src/main/java/viewandmodels/set/Board.java
@@ -1,21 +1,20 @@
 package viewandmodels.set;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.shape.Rectangle;
 
-public class hola {
+public class Board {
     @FXML
     private GridPane chessBoard;
 
+    // Metoda inicjalizacyjna, która jest wywoływana po załadowaniu pliku FXML
     @FXML
-    private void initialize() {
-        // Tworzenie i dodawanie pól planszy
+    public void initialize() {
+        // Wstawianie pól planszy
         for (int row = 0; row < 8; row++) {
             for (int col = 0; col < 8; col++) {
-                Rectangle square = new Rectangle(50, 50); // Utworzenie kwadratu o wymiarach 50x50
+                Rectangle square = new Rectangle(50, 50); // Tworzenie kwadratu o wymiarach 50x50
                 if ((row + col) % 2 == 0) {
                     square.setFill(javafx.scene.paint.Color.WHITE); // Ustawienie koloru białego dla pól parzystych
                 } else {
@@ -24,14 +23,5 @@ public class hola {
                 chessBoard.add(square, col, row); // Dodanie kwadratu do planszy
             }
         }
-    }
-
-    @FXML
-    private Label byeText;
-
-    @FXML
-    protected void onByebyeButtonClick() {
-        // Zmiana tekstu po kliknięciu przycisku
-        byeText.setText("Bye bye to JavaFX Application!");
     }
 }

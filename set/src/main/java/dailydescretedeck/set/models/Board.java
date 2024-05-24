39916924d014c64163ca673a057896ab8db99246
annaszpa <Annaa.szpak@student.uj.edu.pl>
2024-05-24 22:09:29 +0200

    new class feature

--- set/src/main/java/dailydescretedeck/set/models/Board.java
+++ set/src/main/java/dailydescretedeck/set/models/Board.java
@@ -8,6 +8,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import dailydescretedeck.set.services.Feature;
+
 
 public class Board {
     private List<Card> cards = new ArrayList<>();
@@ -126,18 +128,10 @@ public class Board {
         return true;
     }
 
-    public void fill(List<List<Card>> list, List<Card> temp, List<Card> colors, int start) {
-        list.add(new ArrayList<>(temp));
-        for (int i = start; i < colors.size(); i++) {
-            temp.add(colors.get(i));
-            fill(list, temp, colors, i + 1);
-            temp.remove(temp.size() - 1);
-        }
-    }
 
     public List<Card> getSet() {
         List<List<Card>> everything = new ArrayList<>();
-        fill(everything, new ArrayList<>(), cards, 0);
+        Feature.fill(everything, new ArrayList<>(), cards, 0);
         for (List<Card> list : everything) {
             if(isSetOk(list) && list.size() != 0) return list;
         }

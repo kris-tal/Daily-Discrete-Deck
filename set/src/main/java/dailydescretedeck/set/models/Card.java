6993fba412edb69034ad262488eaa88744f831ce
kris-tal <mdmgiglok@gmail.com>
2024-05-24 20:52:17 +0200

    ArrayList -> List

--- set/src/main/java/dailydescretedeck/set/models/Card.java
+++ set/src/main/java/dailydescretedeck/set/models/Card.java
@@ -3,17 +3,18 @@ package dailydescretedeck.set.models;
 import dailydescretedeck.set.views.carddesignes.DefaultCardDesign;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Card {
     private ArrayList<Dots> fields;
     private CardDesign design;
 
-    public Card(ArrayList<Dots> existingFields) {
-        this.fields = existingFields;
+    public Card(List<Dots> existingFields) {
+        this.fields = (ArrayList<Dots>) existingFields;
         this.design = new DefaultCardDesign();
     }
 
-    public ArrayList<Dots> getFields() {
+    public List<Dots> getFields() {
         return this.fields;
     }
     public CardDesign getDesign() {

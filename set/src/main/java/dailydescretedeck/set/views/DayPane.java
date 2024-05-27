2e21e0855b357fa9c53ffb6f4baa808851192766
kris-tal <mdmgiglok@gmail.com>
2024-05-27 11:14:37 +0200

    Update DayPane.java

--- set/src/main/java/dailydescretedeck/set/views/DayPane.java
+++ set/src/main/java/dailydescretedeck/set/views/DayPane.java
@@ -11,7 +11,9 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 
@@ -19,14 +21,21 @@ public class DayPane extends StackPane {
     private VBox data;
     private int setNumber;
     private int endNumber;
-    private Map<Integer, Color> rectColor;
+
+    private Color chooseColor(int sets) {
+        if(sets == 0) return Color.WHITE;
+        if(sets < 4) return Color.rgb(210,230,181);
+        if(sets < 8) return Color.rgb(193,219,155);
+        if(sets < 12) return Color.rgb(177,207,134);
+        if(sets < 18) return Color.rgb(160,193,114);
+        return Color.rgb(142,177,92);
+    }
 
     public DayPane(int dayOfMonth, int sets, int ends) {
-        rectColor = Map.of(0, Color.WHITE, 1, Color.rgb(210,230,181), 2, Color.rgb(193,219,155), 3, Color.rgb(177,207,134), 4, Color.rgb(160,193,114), 5, Color.rgb(142,177,92));
 
         Label dateLabel = new Label(Integer.toString(dayOfMonth));
         dateLabel.setFont(Font.font("System Bold", FontWeight.BOLD, 15));
-        dateLabel.setTextFill(rectColor.get(sets).darker().darker().darker());
+        dateLabel.setTextFill(chooseColor(sets).darker().darker().darker());
         //Label setsLabel = new Label("Sets: " + sets);
         //Label endsLabel = new Label("Ends: " + ends);
         this.data = new VBox(dateLabel);
@@ -34,9 +43,9 @@ public class DayPane extends StackPane {
         Rectangle rect = new Rectangle(40, 40);
         rect.setArcHeight(30);
         rect.setArcWidth(30);
-        rect.setFill(rectColor.get(sets));
+        rect.setFill(chooseColor(sets));
         rect.setStrokeWidth(2);
-        rect.setStroke(rectColor.get(sets).darker());
+        rect.setStroke(chooseColor(sets).darker());
 
         getChildren().addAll(rect, data);
         data.setAlignment(Pos.CENTER);

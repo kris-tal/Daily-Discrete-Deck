2413dad17671dab8411e9c3cd7dc46738398bb62
kris-tal <mdmgiglok@gmail.com>
2024-06-07 13:24:35 +0200

    Update Set.java

--- set/src/main/java/dailydescretedeck/set/Set.java
+++ set/src/main/java/dailydescretedeck/set/Set.java
@@ -3,6 +3,7 @@ package dailydescretedeck.set;
 import dailydescretedeck.set.models.Player;
 import dailydescretedeck.set.models.SimpleBoardState;
 import dailydescretedeck.set.viewmodels.ProfileViewModel;
+import dailydescretedeck.set.viewmodels.Scenes;
 import dailydescretedeck.set.viewmodels.StoreViewModel;
 import dailydescretedeck.set.views.MenuView;
 import dailydescretedeck.set.views.PlayView;
@@ -15,46 +16,15 @@ import javafx.stage.Stage;
 public class Set extends Application {
     private Stage primaryStage;
     private Player player;
-    private StoreViewModel storeViewModel;
 
     @Override
     public void start(Stage stage) {
         this.primaryStage = stage;
-        this.player = new Player("Player1", "password");
-        this.storeViewModel = new StoreViewModel(player);
-
-        showMenu();
-    }
-
-    private void showMenu() {
-        MenuView menuView = new MenuView(this::showPlayView, this::showStoreView, this::showProfileView);
-        Scene scene = new Scene(menuView, 1000, 800);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Menu");
-        primaryStage.show();
-    }
-
-    private void showPlayView() {
-        SimpleBoardState boardState = new SimpleBoardState(7);
-        PlayView playView = new PlayView(boardState, this::showMenu);
-        Scene scene = new Scene(playView, 1000, 800);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Set");
-    }
-
-    private void showProfileView() {
-        ProfileViewModel profileViewModel = new ProfileViewModel();
-        ProfileView profileView = new ProfileView(profileViewModel, this::showMenu);
-        Scene scene = new Scene(profileView, 1000, 800);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Profile");
-    }
-
-    private void showStoreView() {
-        StoreView storeView = new StoreView(storeViewModel, primaryStage, this::showMenu);
-        Scene scene = new Scene(storeView, 1000, 800);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Store");
+        //wczytaj playera, jesli nie ma to okienko z imieniem i stworz nowego
+        this.player = new Player("Player1");
+        Scenes.setPrimaryStage(primaryStage);
+        Scenes scenes = new Scenes();
+        scenes.showMenuView();
     }
 
     public static void main(String[] args) {

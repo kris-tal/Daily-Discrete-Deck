06311a4f073238655544b5fa8fbf629e7f7563bb
Gabriela6666 <pietrasgabriela6@gmail.com>
2024-06-09 23:08:17 +0200

    mvvm

--- set/src/main/java/dailydescretedeck/set/viewmodels/CardViewModel.java
+++ set/src/main/java/dailydescretedeck/set/viewmodels/CardViewModel.java
@@ -1,6 +1,23 @@
 package dailydescretedeck.set.viewmodels;
 
+import dailydescretedeck.set.models.Board;
+import dailydescretedeck.set.models.Card;
+import dailydescretedeck.set.models.Dots;
+
+import java.util.List;
+
 public class CardViewModel {
-    CardViewModel() {
+    private Card card;
+
+    CardViewModel(Card card) {
+        this.card = card;
+    }
+
+    public CardDesign getDesign(){
+        return card.getDesign();
+    }
+
+    public CardViewModel(List<Dots> dots, CardDesign design){
+        this.card = new Card(dots, design);
     }
 }
